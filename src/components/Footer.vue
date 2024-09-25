df285cf51eb565230626f389620bb36e4b08216a
Xondamir Nazrullayev <xondamirnazrullayev@mail.ru>
2024-09-25 16:45:14 +0500

    removing subscribe section

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,20 +4,6 @@
 			<Logo class="footer__logo" />
 			<LinkList class="footer__links" />
 		</div>
-		<div class="footer__main">
-			<h2 class="h2">{{ $t('footer-subscribe') }}</h2>
-			<form class="footer__form" @submit.prevent="submitEmail">
-				<input
-					:placeholder="$t('enter-email')"
-					type="email"
-					required
-					v-model="email"
-					class="footer__input body-1" />
-				<button class="button footer__button" type="submit">
-					{{ isLoading ? $t('sending') : isSent ? $t('sent') : $t('subscribe') }}
-				</button>
-			</form>
-		</div>
 		<div class="footer__bottom">
 			<div class="footer__info">
 				<p class="body-1">{{ $t('address') }}</p>
@@ -29,78 +15,31 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted } from 'vue';
 import Logo from './Icons/Logo.vue';
 import LinkList from './LinkList.vue';
 import Socials from './Socials.vue';
-import env from '@/env';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
-const isLoading = ref(false);
-const isSent = ref(false);
-const email = ref('');
-
-const submitEmail = async () => {
-	email.value = email.value.trim();
-
-	if (!email.value) return;
-
-	isLoading.value = true;
-
-	const text = `
-Type: Subscription
-Email: ${email.value}
-Time: ${Intl.DateTimeFormat('en-GB', {
-		dateStyle: 'short',
-		timeStyle: 'short'
-	}).format(new Date())}
-	`;
-
-	const res = await fetch(`https://api.telegram.org/bot${env.botToken}/sendMessage`, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json'
-		},
-		body: JSON.stringify({ chat_id: env.chat_id, text })
-	});
-	await res.json();
-
-	isSent.value = true;
-	isLoading.value = false;
-	email.value = '';
-};
-
 onMounted(() => {
 	gsap.from('.footer__head', {
-		y: -50,
-		opacity: 0,
-		scrollTrigger: {
-			trigger: '.footer__head',
-			scrub: 1,
-			start: 'center 70%',
-			end: 'bottom center'
-		}
-	});
-	gsap.from('.footer__main', {
-		x: -50,
-		opacity: 0,
-		scrollTrigger: {
-			trigger: '.footer__main',
-			scrub: 1,
-			start: 'top bottom',
-			end: 'bottom bottom'
-		}
-	});
-	gsap.from('.footer__bottom', {
-		y: 50,
+		y: -150,
 		opacity: 0,
 		scrollTrigger: {
 			trigger: '.footer',
-			scrub: 1,
-			start: 'top center',
-			end: 'bottom bottom'
+			start: 'top 80%',
+			toggleActions: 'play none reverse reverse'
+		}
+	});
+	gsap.from('.footer__bottom', {
+		y: 150,
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.footer',
+			start: 'top 80%',
+			toggleActions: 'play none reverse reverse'
 		}
 	});
 });
@@ -143,44 +82,6 @@ onMounted(() => {
 		@media only screen and (max-width: 500px) {
 			order: -1;
 		}
-	}
-	&__button {
-		@media only screen and (max-width: 1000px) {
-			width: 100%;
-		}
-	}
-	&__form {
-		display: flex;
-		flex-wrap: wrap;
-		gap: 24px;
-	}
-	&__input {
-		font-family: var(--font-inter);
-		color: var(--medium-grey);
-		background-color: transparent;
-		padding: 0.7rem 1.2rem;
-		border: 1px solid var(--dark-grey);
-
-		@media only screen and (max-width: 1000px) {
-			width: 100%;
-		}
-		&:user-invalid {
-			border-color: red !important;
-			color: red;
-		}
-		&:focus {
-			border-color: var(--yellow);
-			outline: none;
-		}
-	}
-	&__main {
-		padding: 4.2rem 3.2rem;
-		background-color: rgba(255, 255, 255, 0.05);
-		color: #fff;
-		display: flex;
-		gap: 20px;
-		flex-wrap: wrap;
-		justify-content: space-between;
 	}
 	&__info {
 		display: flex;
